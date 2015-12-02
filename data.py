7281e173d1533203778aebf3c81207ea4971b19b
vivek <keviv93@gmail.com>
2015-12-02 15:37:56 -0600

    data generator

--- data.py
+++ data.py
@@ -1,35 +1,42 @@
 
-import sys
 import os
 import re
+
+EMPTY = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
 log = os.popen('git log').read().split('\n')
 log = list(filter(lambda x: re.match('commit [0-9a-f]{5,40}',x) != None or x[:6] == 'Author', log))
 log = list(map(lambda x: x.split(' ')[1].lower() if x[:6] == 'Author' else x.split(' ')[1], log))
 log = list(zip(log[1:len(log):2], log[0:len(log):2]))
-activity = {}
-for l in log:
-	out = os.popen('git diff '+l[1]+' --numstat').read().split('\n')
-	for o in out:
-		print(l)
-		print(o)
+activity = dict(map(lambda x: (x[0], {}), log))
+for i in range(len(log)):
+	if i < len(log)-1:
+		out = os.popen('git diff '+log[i][1]+' '+log[i+1][1]+' --numstat').read().split('\n')
+	else:
+		out = os.popen('git diff '+log[i][1]+' '+EMPTY+' --numstat').read().split('\n')
+	user = log[i][0]
+	for o in out[:-1]:
+		ol = o.split('\t')
+		changes = int(ol[0])+int(ol[1])
+		k = '/'+ol[2]
+		if k in activity[user]:
+			activity[user][k] += changes
+		else:
+			activity[user][k] = changes
 
+# print(activity)
 
-
-# [print(l) for l in log]
+# for l in log: print(l)
 
 rootDir = '.'
 fileList = []
 
-for dir_, _, files in os.walk(rootDir):
-    for fileName in files:
-        relDir = os.path.relpath(dir_, rootDir)
-        relFile = os.path.join(relDir, fileName)
-        fileList.append(relFile)
+cmd = """ls -R """+rootDir+""" | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }'"""
+fileList = os.popen(cmd).read().split('\n')
+fileList = list(map(lambda x: x[1:], fileList))
+fileList = sorted(fileList, key=lambda x: x.count('/'))[1:]
 
-fileList = list(filter (lambda x: x.split('/')[0] != '.git', fileList))
-fileList = map(lambda x: x[1:] if x[0] == '.' else '/'+x, fileList)
-[print(f) for f in fileList]
+# for f in fileList: print(f)
 
 class Node:
 
@@ -42,7 +49,8 @@ def getNode(node, path):
 	if node.path == path: return node
 	parent = None
 	for c in node.files:
-		parent = get_node(c, path)
+		parent = getNode(c, path)
+		if parent != None: break
 	return parent
 
 def addNode(node, childPath, parentPath):
@@ -52,7 +60,43 @@ def addNode(node, childPath, parentPath):
 
 def populateTree(root, data):
 	for d in data:
-		idx = d.path.rfind('/')
+		idx = d.rfind('/')
 		parent = '/'
-		if (idx > 0) parent = data.path[:idx]
-		addNode(root, d.path, parent)
+		if (idx > 0): parent = d[:idx]
+		addNode(root, d, parent)
+
+def combineDicts(d1, d2):
+	result = {}
+	for k in d1: result[k] = d1[k]
+	for k in d2:
+		if k in result:
+			result[k] += d2[k]
+		else:
+			result[k] = d2[k]
+	return result
+
+def extractActivity(path):
+	result = {}
+	for user in activity:
+		if path in activity[user]:
+			result[user] = activity[user][path]
+	return result
+
+def getContributions(node):
+	if len(node.files) == 0:
+		node.contributions = extractActivity(node.path)
+		return node.contributions
+	for c in node.files:
+		node.contributions = combineDicts(node.contributions, getContributions(c))
+	return node.contributions
+
+root = Node('/')
+populateTree(root, fileList)
+getContributions(root)
+
+import json
+import pprint
+
+json = json.dumps(root, default=lambda o: o.__dict__, sort_keys=False, indent=4, separators=(',', ': '))
+
+print(json)
